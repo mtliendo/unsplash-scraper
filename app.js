bc3e9ce89fea5f196f3b57e9a7e1f6f9609b44ca
Michael Liendo <model3volution@gmail.com>
2018-04-05 01:05:20 -0500

    feat: working project

---
+++ app.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+    cheerio = require('cheerio')
+    axios = require('axios')
+
+
+const SEARCH_TERM = process.env.SEARCH_TERM || 'turtles'
+
+//1. fetch the site
+function fetchUnsplashData() {
+    return axios.get(`https://unsplash.com/search/photos/${SEARCH_TERM}`)
+        .then(res => res.data)
+}
+
+//2. grabImages
+function grabImages(data) {
+    return new Promise((resolve, reject) => {
+        if(data) {
+            const $ = cheerio.load(data)
+            const imageLinks = $('a[title="Download photo"]').map((index, image) => {
+                return $(image).attr('href')
+            })
+            resolve(imageLinks)
+        }
+    })
+}
+
+//3. saveImages
+function saveImages(images) {
+    images.map((index, image) => {
+        axios({
+            method: 'get',
+            responseType: 'stream',
+            url: image
+        }).then((item) => {
+            item.data.pipe(fs.createWriteStream(`./images/${SEARCH_TERM}${index}.jpg`))
+        })
+    })
+}
+
+//4. put them all together
+fetchUnsplashData()
+.then(grabImages)
+.then(saveImages)
